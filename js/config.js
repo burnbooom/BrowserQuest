5c52f813c87fc42105e5fdf3d20cea00e12cce85
burnbooom <uzl4061@edu.kunskapsskolan.se>
2026-03-12 11:58:02 +0100

    Refactor config.js to use renderConfig structure

--- js/config.js
+++ js/config.js
@@ -1,20 +1,14 @@
-
-define(['text!../config/config_build.json'],
-function(build) {
-    var config = {
-        dev: { host: "localhost", port: 8000, dispatcher: false },
-        build: JSON.parse(build)
+define([], function() {
+    var renderConfig = {
+        host: "browserquest-server.onrender.com",
+        port: 443,
+        secure: true,
+        dispatcher: false
     };
     
-    //>>excludeStart("prodHost", pragmas.prodHost);
-    require(['text!../config/config_local.json'], function(local) {
-        try {
-            config.local = JSON.parse(local);
-        } catch(e) {
-            // Exception triggered when config_local.json does not exist. Nothing to do here.
-        }
-    });
-    //>>excludeEnd("prodHost");
-    
-    return config;
+    return {
+        dev: renderConfig,
+        build: renderConfig,
+        local: renderConfig
+    };
 });
